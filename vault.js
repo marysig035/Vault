dfb202c857c36e27481d4a7c00aa4cfff7a8ebce
Mary Sigmon <marysigmon@Amys-MacBook-Pro.local>
2023-11-06 20:14:06 -0500

    added constants and message

---
+++ vault.js
@@ -0,0 +1,6 @@
+// variables and arithmetic operators:
+const lock1 = 5 * 2;
+const lock2 = 10 + 30;
+const lock3 = 40 - 1;
+const message = `You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${lock1} + ${lock2} + ${lock3}`
+alert(message);
