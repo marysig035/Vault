f3529b41c3c0557edebb96104c1652ccc1c764e7
Mary Sigmon <marysigmon@Amys-MacBook-Pro.local>
2023-11-06 20:25:54 -0500

    working on alert

--- vault.js
+++ vault.js
@@ -1,6 +1,16 @@
+console.log('You have received this message because you have been chosen to open an important vault. Here is the secret combination:');
+
 // variables and arithmetic operators:
 const lock1 = 5 * 2;
 const lock2 = 10 + 30;
 const lock3 = 40 - 1;
-const message = `You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${lock1} + ${lock2} + ${lock3}`
-alert(message);
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+const final = `You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${lock1} ${lock2} ${lock3}`
+
+console.log(lock1, lock2, lock3);
+
+function displayAlert(){
+    alert(final);
+}
+
+alert(final);
